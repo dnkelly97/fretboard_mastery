7ad85103204d6b97ab205fd1739730ca0f590b25
dnkelly97 <dnkelly97@gmail.com>
2021-12-01 00:11:54 -0600

    Adapted example pitch detection code from ml5 github (linked in detect_pitch.js) to detect pitch for 10 seconds at a time. Added an html button to start this process.

--- guitar_exercises/fretboard_exercises/static/fretboard_exercises/detect_pitch.js
+++ guitar_exercises/fretboard_exercises/static/fretboard_exercises/detect_pitch.js
@@ -1,4 +1,37 @@
 
-function runChallenge(){
-    $('#result').html("king yeetus");
+// This code is adapted from the ml5 library example found at this site:
+// https://github.com/ml5js/ml5-library/blob/main/examples/javascript/PitchDetection/PitchDetection/sketch.js
+
+let audioContext;
+let mic;
+let pitch;
+
+async function startPitch() {
+  audioContext ||= new AudioContext();
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+  pitch = ml5.pitchDetection('http://localhost:8000/static/fretboard_exercises/model', audioContext , stream, modelLoaded);
 }
+
+function modelLoaded() {
+  document.querySelector('#status').textContent='Model Loaded';
+  let pitchInterval;
+  setTimeout(() => {
+    clearInterval(pitchInterval);
+    document.querySelector('#result').textContent = 'Done';
+    stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+  }, 10000);
+  pitchInterval = setInterval(getPitch, 50);
+}
+
+function getPitch(start) {
+  pitch.getPitch(function(err, frequency) {
+    if (frequency) {
+      document.querySelector('#result').textContent = frequency;
+    } else {
+      document.querySelector('#result').textContent = 'No pitch detected';
+    }
+  })
+}
+
