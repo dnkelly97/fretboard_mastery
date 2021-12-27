bdc4c2aea7495c4844e29d3814ea9e4f0cb7b11e
dnkelly97 <dnkelly97@gmail.com>
2021-12-27 17:41:27 -0600

    Added to unit test to test functionality for returning random note within bounds specified from frontend form input

--- guitar_exercises/test/test_fretboard_exercises.py
+++ guitar_exercises/test/test_fretboard_exercises.py
@@ -7,10 +7,13 @@ class TestFretboardExerciseViews:
 
     def test_note_xhr(self, client):
         url = reverse('new_note')
-        response = client.get(url)
-        note_info = json.loads(response.content)
-        assert note_info['frequency']
-        assert note_info['note']
-        assert note_info['string']
+        request_data = {'strings': [5, 6], 'notes': ['A', 'B', 'C']}
+        # after 100 trials, probability that a wrong note or string was possible but didn't happen is vanishingly small
+        for i in range(100):
+            response = client.post(url, request_data)
+            note_info = json.loads(response.content)
+            assert note_info['frequency']
+            assert note_info['note'] in request_data['notes']
+            assert note_info['string'] in request_data['strings']
         # pdb.set_trace()
 
