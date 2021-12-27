42e4d205392dd1adf9deef84779b93aae1a20117
dnkelly97 <dnkelly97@gmail.com>
2021-12-26 21:26:07 -0600

    Created new URL and view for /fretboard/new_note ajax request to make failing test pass

--- guitar_exercises/fretboard_exercises/urls.py
+++ guitar_exercises/fretboard_exercises/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('new_note', views.get_new_note, name='new_note')
 ]
--- guitar_exercises/fretboard_exercises/views.py
+++ guitar_exercises/fretboard_exercises/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'fretboard_exercises/index.html', {'note': 'high e', 'frequency': 329.6})
+
+def get_new_note(request):
+    return JsonResponse({})
 
