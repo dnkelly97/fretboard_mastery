c9ee2c75ae48049619d669a1ed26840f45ed9702
dnkelly97 <dnkelly97@gmail.com>
2021-12-26 21:33:14 -0600

    Added to test_note_xhr unit test to assert the json response has the note, string, and frequency information. Users will be told what note to play on what string, and the frequency will be used as the target for the pitch detection to verify the user is playing the right note.

--- guitar_exercises/test/test_fretboard_exercises.py
+++ guitar_exercises/test/test_fretboard_exercises.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
-# import pdb
+import json
+import pdb
 
 
 class TestFretboardExerciseViews:
@@ -7,4 +8,9 @@ class TestFretboardExerciseViews:
     def test_note_xhr(self, client):
         url = reverse('new_note')
         response = client.get(url)
+        note_info = json.loads(response.content)
+        assert note_info[frequency]
+        assert note_info[note]
+        assert note_info[string]
+        # pdb.set_trace()
 
