40790ceb8ceedd2650944727a5dc6167fdf716e9
dnkelly97 <dnkelly97@gmail.com>
2021-12-26 21:35:48 -0600

    added quotes to dict keys

--- guitar_exercises/test/test_fretboard_exercises.py
+++ guitar_exercises/test/test_fretboard_exercises.py
@@ -9,8 +9,8 @@ class TestFretboardExerciseViews:
         url = reverse('new_note')
         response = client.get(url)
         note_info = json.loads(response.content)
-        assert note_info[frequency]
-        assert note_info[note]
-        assert note_info[string]
+        assert note_info['frequency']
+        assert note_info['note']
+        assert note_info['string']
         # pdb.set_trace()
 
