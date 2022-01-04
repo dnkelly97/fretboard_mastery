908756bcff1372e22703c834bcc2a1473fa490a0
dnkelly97 <dnkelly97@gmail.com>
2022-01-04 00:31:26 -0600

    finished get_new_note view, it now passes test. Also added a variable INSTRUMENT_MODEL_MAP. It's a dict that maps the instrument from the url to the correct model for the instrument.

--- guitar_exercises/fretboard_exercises/views.py
+++ guitar_exercises/fretboard_exercises/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+import random
+from django.db.models import Q
+from .models import Note, GuitarFretboardLocation
 # Create your views here.
 
 
-def index(request):
+INSTRUMENT_MODEL_MAP = {'guitar': GuitarFretboardLocation}
+
+
+def index(request, instrument):
     return render(request, 'fretboard_exercises/index.html', {'note': 'high e', 'frequency': 329.6})
 
-def get_new_note(request):
-    return JsonResponse({'note': 'E', 'string': 5, 'frequency': 230})
+
+def get_new_note(request, instrument):
+    allowed_strings = request.POST.getlist('strings')
+    allowed_notes = request.POST.getlist('notes')
+    fretboard_location = INSTRUMENT_MODEL_MAP[instrument].objects.filter(note__name__in=allowed_notes, string__in=allowed_strings).order_by('?').first()
+    return JsonResponse({'note': fretboard_location.note.name, 'string': fretboard_location.string, 'frequency': fretboard_location.note.frequency})
 
