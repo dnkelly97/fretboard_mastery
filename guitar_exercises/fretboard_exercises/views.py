f9d24f33c4e3c80f580c3294e64c34a70e85366e
dnkelly97 <dnkelly97@gmail.com>
2022-01-09 18:02:46 -0600

    Changed fret constraint to be <= 11, since note ambiguity for a given string is still possible for 12 frets plus the open strings.

--- guitar_exercises/fretboard_exercises/views.py
+++ guitar_exercises/fretboard_exercises/views.py
@@ -16,7 +16,7 @@ def index(request, instrument):
 def get_new_note(request, instrument):
     allowed_strings = list(map(str, request.POST.getlist('strings')))
     allowed_notes = request.POST.getlist('notes')
-    fretboard_location = INSTRUMENT_MODEL_MAP[instrument].objects.filter(note__name__in=allowed_notes, string__in=allowed_strings, fret__lte=12).order_by('?').first()
+    fretboard_location = INSTRUMENT_MODEL_MAP[instrument].objects.filter(note__name__in=allowed_notes, string__in=allowed_strings, fret__lte=11).order_by('?').first()
     return JsonResponse({'note': fretboard_location.note.name, 'string': fretboard_location.string, 'frequency': fretboard_location.note.frequency})
 
 # TODO add timer and score count to UI
