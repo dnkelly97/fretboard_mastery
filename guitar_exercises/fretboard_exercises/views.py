f2b5396843532dbea16dcd5a12d8e9666b2e420d
dnkelly97 <dnkelly97@gmail.com>
2022-01-14 23:38:45 -0600

    Modified get_new_note view to be able to handle previous_note and previous_string data. If given, the returned note and string will not be the same as those given. Both, one, or neither of the arguments can be given. Associated unit tests are now passing.

--- guitar_exercises/fretboard_exercises/views.py
+++ guitar_exercises/fretboard_exercises/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.utils.datastructures import MultiValueDictKeyError
 import random
 from django.db.models import Q
 from .models import Note, GuitarFretboardLocation
@@ -16,10 +17,18 @@ def index(request, instrument):
 def get_new_note(request, instrument):
     allowed_strings = list(map(str, request.POST.getlist('strings')))
     allowed_notes = request.POST.getlist('notes')
+    try:
+        previous_note = request.POST['previous_note']
+        allowed_notes.remove(previous_note)
+    except MultiValueDictKeyError:
+        pass
+    try:
+        previous_string = request.POST['previous_string']
+        allowed_strings.remove(previous_string)
+    except MultiValueDictKeyError:
+        pass
     if not allowed_notes or not allowed_strings:
         return JsonResponse({'status': False, 'message': 'At least one note and one string must be selected'}, status=406)
     fretboard_location = INSTRUMENT_MODEL_MAP[instrument].objects.filter(note__name__in=allowed_notes, string__in=allowed_strings, fret__lte=11).order_by('?').first()
     return JsonResponse({'note': fretboard_location.note.name, 'string': fretboard_location.string, 'frequency': fretboard_location.note.frequency})
 
-# TODO different response for case where no strings or notes are selected by form
-# TODO make it so the same note doesn't get randomly selected twice in a row
