249173de80a1ab2170076feb462264db742482f1
dnkelly97 <dnkelly97@gmail.com>
2025-11-09 23:20:23 -0700

    more refactoring for readability

--- guitar_exercises/fretboard_exercises/static/fretboard_exercises/detect_pitch.js
+++ guitar_exercises/fretboard_exercises/static/fretboard_exercises/detect_pitch.js
@@ -7,12 +7,82 @@ let stream;
 let exerciseTimeInMs = 6000;
 let target_frequency;
 let score = 0;
-
 const TARGET_FREQUENCY_MARGIN = 5; //TODO what should allowable margin be?
+
+$(document).ready(function(){
+  overrideFormSubmit();
+  $("#alert-warning").text("");
+  setup().then(pauseAudio);
+});
+
+function overrideFormSubmit()
+{
+  $('#new_note_form').submit(function(e){
+    e.preventDefault();
+    if(formIsValid()){
+      runChallenge();
+    }
+    else{
+      $("#alert-warning").text("At least one string and at least one note must be selected to begin challenge.");
+    }
+  });
+}
+
+async function setup() {
+  audioContext = new AudioContext();
+  analyzerNode = audioContext.createAnalyser();
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  audioContext.createMediaStreamSource(stream).connect(analyzerNode);
+  pitchDetector = PitchDetector.forFloat32Array(analyzerNode.fftSize);
+  enableForm();
+  console.log('setup complete');
+}
+
+function enableForm()
+{
+  $('#begin_button').prop('disabled', false);
+  //$('#notes_fieldset')[0].disabled = false;
+  //$('#strings_fieldset')[0].disabled = false;
+}
+
+function disableForm()
+{
+  $('#begin_button').prop('disabled', true);
+  //$('#notes_fieldset')[0].disabled = true;
+  //$('#strings_fieldset')[0].disabled = true;
+}
+
+function pauseAudio(){
+  audioContext.suspend().then(() => {
+    console.log('Audio Context Suspended');
+  });
+}
+
+async function resumeAudio(){
+  if(audioContext.state === 'suspended'){
+    await audioContext.resume();
+    console.log('Audio Context Resumed');
+  }
+}
 
 function formIsValid(){
   let data = $("#new_note_form").serialize();
   return data.includes("notes=") && data.includes("strings=");
+}
+
+async function runChallenge(){
+  await challengeSetup();
+  let startTime = Date.now();
+  await runPitchDetection(startTime)
+  challengeTeardown(startTime);
+}
+
+async function challengeSetup()
+{
+  await resumeAudio();
+  disableForm();
+  score = 0;
+  getNewNote();
 }
 
 function getNewNote(){
@@ -35,89 +105,37 @@ function getNewNote(){
   });
 }
 
-$(document).ready(function(){
-  $('#new_note_form').submit(function(e){
-    e.preventDefault();
-    beginChallenge();
-  });
-  $("#alert-warning").text("");
-  setup().then(pauseAudio);
-});
-
-async function beginChallenge(){
-  if(formIsValid()){
-    await resumeAudio();
-    disableForm();
-    score = 0;
-    getNewNote();
-    startPitchDetection().then(pauseAudio);
-  }
-  else{
-    $("#alert-warning").text("At least one string and at least one note must be selected to begin challenge.");
-  }
-}
-
-function pauseAudio(){
-  audioContext.suspend().then(() => {
-    console.log('Audio Context Suspended');
-  });
-}
-
-async function resumeAudio(){
-  if(audioContext.state === 'suspended'){
-    await audioContext.resume();
-    console.log('Audio Context Resumed');
-  }
-}
-
-async function setup() {
-  audioContext = new AudioContext();
-  analyzerNode = audioContext.createAnalyser();
-  stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-  audioContext.createMediaStreamSource(stream).connect(analyzerNode);
-  pitchDetector = PitchDetector.forFloat32Array(analyzerNode.fftSize);
-  enableForm();
-  console.log('setup complete');
-}
-
-async function startPitchDetection(){
-  let startTime = Date.now();
+async function runPitchDetection(startTime)
+{
   console.log(`starting at ${new Date(startTime).toISOString()}`);
-
-  while (Date.now() - startTime < exerciseTimeInMs) {
+  while (challengeTimerNotElapsed(startTime)) 
+  {
     let frequency = getPitch();
     showUpdatedTimeLeft(startTime);
     showUpdatedResult(frequency);
-    if(correctNotePlayed(frequency)){
-        getNewNote();
-        score++;
+    if(correctNotePlayed(frequency))
+    {
+      getNewNote();
+      score++;
     }
     await new Promise(resolve => setTimeout(resolve, 100));
   }
-
-  console.log(`Finished at ${new Date().toISOString()}`);
-  showUpdatedTimeLeft(startTime);
-  showUpdatedResult('Done');
-  enableForm();
 }
 
-function enableForm()
+function challengeTimerNotElapsed(startTime)
 {
-  $('#begin_button').prop('disabled', false);
-  //$('#notes_fieldset')[0].disabled = false;
-  //$('#strings_fieldset')[0].disabled = false;
+  return Date.now() - startTime < exerciseTimeInMs;
 }
 
-function disableForm()
+function getPitch() 
 {
-  $('#begin_button').prop('disabled', true);
-  //$('#notes_fieldset')[0].disabled = true;
-  //$('#strings_fieldset')[0].disabled = true;
-}
-
-function correctNotePlayed(frequency)
-{
-  return Math.abs(frequency - target_frequency) < TARGET_FREQUENCY_MARGIN;
+  let input = new Float32Array(analyzerNode.fftSize);
+  analyzerNode.getFloatTimeDomainData(input);
+  console.log(input.slice(0, 10));
+  const [frequency, clarity] = pitchDetector.findPitch(input, audioContext.sampleRate);
+  console.log(`clarity: ${clarity}`);
+  console.log(`frequency: ${frequency}`);
+  return clarity > 0.85 ? frequency : "No pitch detected";
 }
 
 function showUpdatedTimeLeft(startTime)
@@ -131,12 +149,16 @@ function showUpdatedResult(result)
   $('#result').text(result);
 }
 
-function getPitch() {
-  let input = new Float32Array(analyzerNode.fftSize);
-  analyzerNode.getFloatTimeDomainData(input);
-  console.log(input.slice(0, 10));
-  const [frequency, clarity] = pitchDetector.findPitch(input, audioContext.sampleRate);
-  console.log(`clarity: ${clarity}`);
-  console.log(`frequency: ${frequency}`);
-  return clarity > 0.85 ? frequency : "No pitch detected";
+function correctNotePlayed(frequency)
+{
+  return Math.abs(frequency - target_frequency) < TARGET_FREQUENCY_MARGIN;
+}
+
+function challengeTeardown(startTime)
+{
+  console.log(`Finished at ${new Date().toISOString()}`);
+  showUpdatedTimeLeft(startTime);
+  showUpdatedResult('Done');
+  enableForm();
+  pauseAudio();
 }
